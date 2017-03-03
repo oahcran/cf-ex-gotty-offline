f786281278c0da8a3f6ebe2dc36c8312ff19b1cc
Chao Ran <oahcran@users.noreply.github.com>
2017-03-03 16:22:07 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ echo -n "Downloading gotty..."
 cd "./gotty"
 curl -s -L -o "gotty_linux_amd64.tar.gz" "https://github.com/yudai/gotty/releases/download/v0.0.13/gotty_linux_amd64.tar.gz"
 tar zxf "gotty_linux_amd64.tar.gz"
+chmod +x ./gotty/gotty
 rm "gotty_linux_amd64.tar.gz"
 cd ".."
 ls -la "./gotty/"
